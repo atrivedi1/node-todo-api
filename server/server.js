ccee263324a92f2c4fbf7c2bd6ce9699caf7eacc
atrivedi1 <akash.trivedi1@gmail.com>
2018-10-03 11:39:43 -0700

    add get route to server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ var app = express();
 
 app.use(bodyParser.json());
 
+
+//add a todo
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -22,6 +24,20 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+//get a todo
+app.get('/todos', (req,res) => {
+	Todo.find()
+		.then((todos) => {
+			res.send({todos});
+		}, (e) => {
+			res.status(400).send(e);
+		})
+
+})
+
+
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
