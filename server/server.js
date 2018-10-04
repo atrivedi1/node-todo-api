ac32eb91b1fb39f76d17f205cb1f51126d3b6401
atrivedi1 <akash.trivedi1@gmail.com>
2018-10-03 19:11:48 -0700

    modify server.js file for heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
-//library imports
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-//local imports
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -13,8 +11,6 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-
-//add a todo
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -27,41 +23,34 @@ app.post('/todos', (req, res) => {
   });
 });
 
-//get a todo
-app.get('/todos', (req,res) => {
-	Todo.find()
-		.then((todos) => {
-			res.send({todos});
-		}, (e) => {
-			res.status(400).send(e);
-		})
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
-})
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
-//get a todo by id
-app.get('/todos/:id', (req,res) => {
-	let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-	if(!ObjectID.isValid(id)) {
-		return res.status(404).send('id passed in is not in a valid format')
-	}
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
 
-	Todo.findById(req.params.id)
-		.then((todo) => {
-			if(!todo) {
-				return res.status(404).send('todo with this id not found');
-			}
-
-			res.send({todo});
-		})
-		.catch((e) => {
-			res.status(400).send();
-		})
-})
-
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
